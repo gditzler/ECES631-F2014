71e592af924d5c5d3817f50f1dbb714f6aad5148
Gregory Ditzler <gregory.ditzler@gmail.com>
2014-09-25 00:00:30 -0400

    updated presentation and fixed the figure captions on the convolution demo

--- w1-intro/src/conv_demo.m
+++ w1-intro/src/conv_demo.m
@@ -40,7 +40,7 @@ hold on;
 box on;
 plot(n, u(n-d), 'bo', 'LineWidth', 2)
 plot(n, u(n), 'ro', 'LineWidth', 2)
-legend({'u[n]', 'a^n u[n]'})
+legend({'u[n-3]', 'u[n]'})
 set(gca, 'fontsize', 24)
 
 h4 = figure;
@@ -49,7 +49,7 @@ box on;
 plot(n, u(n-d), 'bo', 'LineWidth', 2)
 plot(n, u(-n), 'go', 'LineWidth', 2)
 plot(m, h/12, 'r*', 'LineWidth', 2)
-legend({'u[n]', 'a^n u[n] flipped','conv(x,f)/12'},'Location','best')
+legend({'u[n]', 'u[n-3] flipped','conv(x,f)/12'},'Location','best')
 set(gca, 'fontsize', 24)
 
 saveas(h3,'eps/conv_02.eps','eps2c')
